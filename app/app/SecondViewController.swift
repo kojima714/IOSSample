f0311a93dc06bc89f154034bddd6ff2447919739
kojima714 <ykojima@itfor.co.jp>
2017-12-27 17:08:41 +0900

    make podfile and change UI

--- app/app/SecondViewController.swift
+++ app/app/SecondViewController.swift
@@ -6,4 +6,90 @@
 //  Copyright © 2017年 小嶋雄大. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class SecondViewController: UIViewController {
+    let titleName: String
+    
+    init(titleName: String) {
+        self.titleName = titleName
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private lazy var container: UIView = {
+        let container = UIView()
+        container.backgroundColor = UIColor.yellow
+        return container
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = titleName
+        self.view.addSubview(container)
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+final class Nav2ViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Nav2"
+        view = tableView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension Nav2ViewController: UITableViewDelegate {
+    
+}
+
+extension Nav2ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+final class MainTabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc = Nav1ViewController()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        let nv = UINavigationController(rootViewController: vc)
+        let vc2 = Nav2ViewController()
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+        let nv2 = UINavigationController(rootViewController: vc2)
+        setViewControllers([nv, nv2], animated: false)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- app/app/ViewController.swift
+++ app/app/ViewController.swift
@@ -7,53 +7,45 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
-    let add : UIButton = UIButton()
+final class Nav1ViewController: UIViewController {
+    private lazy var container: UIView = {
+        let container = UIView()
+        container.backgroundColor = UIColor.gray
+        
+        let button = UIButton(type: .system)
+        container.addSubview(button)
+        button.setTitle("push", for: .normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = UIColor.blue
+        button.addTarget(self, action: #selector(onTappedPush(_:)), for: .touchUpInside)
+        button.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+            make.center.equalTo(container)
+        }
+        return container
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red // 背景を赤に
         
-        // ボタンの設定
-        add.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        add.backgroundColor = UIColor.blue
-        
-        add.layer.masksToBounds = true
-        // タイトルを設定する（通常時）
-        add.setTitle("Test", for: UIControlState.normal)
-        add.setTitleColor(UIColor.purple, for: UIControlState.normal)
-        
-        // タイトルを設定する（ボタンがハイライトされた時）
-        add.setTitle("Push", for: UIControlState.highlighted)
-        add.setTitleColor(UIColor.black, for: UIControlState.highlighted)
-        
-        // コーナーの半径を設定する
-        add.layer.cornerRadius = 10.0
-        
-        // ボタンの位置を指定する
-        add.layer.position = CGPoint(x: self.view.frame.width / 2, y: 400)
-        // タグを設定する
-        add.tag = 1
-        
-        // イベントを追加する
-        add.addTarget(self, action: #selector(self.onClickadd(sender:)), for: .touchUpInside)
-        
-        // ボタンをViewに追加する
-        self.view.addSubview(add)
-        
+        navigationItem.title = "Nav1"
+        self.view.addSubview(container)
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @objc func onClickadd(sender: UIButton) {
-
-        print("success")
+    
+    @objc func onTappedPush(_ sender: UIButton) {
+        print(sender)
+        let vc = SecondViewController(titleName: "second")
+        navigationController?.pushViewController(vc, animated: true)
     }
-
 }
-
